36e4631f890b0a89d6e544bb00b3dfdf4dc7b97f
pradippaulLTIM <pradip.paul@ltimindtree.com>
2024-01-28 10:00:31 +0530

    Added project skeleton

---
+++ DemoJokesAppLTIM/DemoJokesAppLTIMApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoJokesAppLTIMApp.swift
+//  DemoJokesAppLTIM
+//
+//  Created by 61092893 on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoJokesAppLTIMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
