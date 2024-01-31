8b7fff1001731671927fa7b6a2a5b2eede03b22c
pradippaulLTIM <pradip.paul@ltimindtree.com>
2024-01-31 20:25:40 +0530

    Fix some common bottlenecks

--- DemoJokesAppLTIM/Presentation/JokesListView.swift
+++ DemoJokesAppLTIM/Presentation/JokesListView.swift
@@ -9,27 +9,33 @@ import SwiftUI
 
 struct JokesListView: View {
     @StateObject var vm = JokesListViewModel()
-
+    
+    func refreshList() async {
+        // asyncronously refresh your list here
+        await vm.getJokes()
+    }
+    
     var body: some View {
-          List{
-              ForEach(vm.jokes) { jokes in
-                  HStack{
-                      Text("\(jokes.joke)")
-                  }
-              }
-          }
-          .task {
-              await vm.getJokes()
-          }
-          .listStyle(PlainListStyle())
-          .navigationTitle("Jokes")
-          
-      }
+        VStack {
+            Text("Jokes").font(.headline)
+            List{
+                ForEach(vm.jokes) { jokes in
+                    HStack{
+                        Text("\(jokes.joke)")
+                    }
+                }
+            }
+            .listStyle(.insetGrouped)
+            .task {
+                await vm.getJokes()
+            }
+            .refreshable {
+                await refreshList()
+            }
+        }
+    }
 }
 
 #Preview {
-    NavigationView{
-        Spacer()
         JokesListView()
-    }.navigationViewStyle(StackNavigationViewStyle())
 }
---
+++ DemoJokesAppLTIMTests/JokesListViewModelUnitTest.swift
@@ -0,0 +1,15 @@
+//
+//  JokesListViewModelUnitTest.swift
+//  DemoJokesAppLTIMTests
+//
+//  Created by 61092893 on 29/01/24.
+//
+
+import XCTest
+@testable import DemoJokesAppLTIM
+
+final class JokesListViewModelUnitTest: XCTestCase {
+
+  
+
+}
