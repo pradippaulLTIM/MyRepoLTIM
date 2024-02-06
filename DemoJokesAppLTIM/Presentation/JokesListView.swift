b932efbdbf4c74a33143c3444a9c678068996e67
pradippaulLTIM <pradip.paul@ltimindtree.com>
2024-02-06 23:02:49 +0530

    Code refactor

--- DemoJokesAppLTIM/Presentation/JokesListView.swift
+++ DemoJokesAppLTIM/Presentation/JokesListView.swift
@@ -18,7 +18,9 @@ struct JokesListView: View {
     var body: some View {
         VStack {
             // ActivityIndicator(isAnimating: $vm.loading, style: .large)
-            Text("Jokes").font(.headline)
+            Text("Jokes").font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
                 .accessibility(label: Text("Jokes List"))
             List{
                 ForEach(vm.jokes) { jokes in
@@ -41,17 +43,7 @@ struct JokesListView: View {
                 }
             }) {
                 Text("Refresh")
-                    .fontWeight(.bold)
-                    .font(.title)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .padding(10)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 40)
-                            .stroke(Color.blue, lineWidth: 5)
-                    )
+                    .ButtonStyle()
             }
             .accessibilityLabel("Refresh")
             .accessibilityAddTraits(.isButton)
@@ -62,4 +54,27 @@ struct JokesListView: View {
 
 #Preview {
         JokesListView()
+}
+
+struct ButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .fontWeight(.bold)
+            .font(.title)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(30)
+            .foregroundColor(.white)
+            .padding(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 30)
+                    .stroke(Color.blue, lineWidth: 5)
+            )
+    }
+}
+
+extension View {
+    func ButtonStyle() -> some View {
+        modifier(ButtonModifier())
+    }
 }
